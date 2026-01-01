0a1bed97c841aaaec95e997ed7cbebed40b5403f
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-01-01 16:42:07 +0700

    initial commit

---
+++ dots/dot_config/fish/readonly_config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    set -g fish_greeting
+
+    alias hx="helix"
+    alias lg="lazygit"
+
+    fzf --fish | source
+    starship init fish | source
+end
---
+++ pkgs/laptop.sh
@@ -0,0 +1,139 @@
+_aur_helper="yay"
+_packages=(
+    # base
+    "base"
+    "base-devel"
+    "linux"
+    "linux-firmware"
+    "sof-firmware"
+    "efibootmgr"
+    "grub"
+    "zram-generator"
+    "yay-bin"
+    "pacman-contrib"
+
+    # network
+    "networkmanager"
+
+    # audio
+    "rtkit"
+    "pipewire"
+    "pipewire-audio"
+    "pipewire-alsa"
+    "pipewire-pulse"
+    "pipewire-jack"
+    "wireplumber"
+
+    # bluetooth
+    "bluez"
+    "bluez-utils"
+
+    # graphics
+    "mesa"
+    "mesa-utils"
+    "vulkan-icd-loader"
+    "vulkan-tools"
+    ## amd
+    "vulkan-radeon"
+    ## nvidia
+    "nvidia-open"
+    "nvidia-utils"
+    "nvidia-prime"
+
+    # misc
+    "gnome-keyring"
+    "udisks2"
+    "gvfs"
+
+    # shells
+    "bash"
+    "bash-completion"
+    "bash-language-server"
+    "fish"
+    "fish-lsp"
+    "starship"
+    "fzf"
+
+    # vcs
+    "git"
+    "lazygit"
+    "openssh"
+
+    # xdg
+    "xdg-utils"
+    "xdg-user-dirs"
+    "xdg-desktop-portal"
+    "xdg-desktop-portal-gtk"
+    "xdg-desktop-portal-hyprland"
+
+    # gtk & qt
+    "gtk3"
+    "gtk4"
+    "qt5-wayland"
+    "qt6-wayland"
+
+    # fonts
+    "fontconfig"
+    "noto-fonts"
+    "noto-fonts-cjk"
+    "noto-fonts-emoji"
+    "noto-fonts-extra"
+    "ttf-liberation"
+    "ttf-0xproto-nerd"
+
+    # icons & color schemes
+    "bibata-cursor-theme-bin"
+    "papirus-icon-theme"
+    "materia-gtk-theme"
+    "materia-kde"
+    "kvantum-theme-materia"
+
+    # theme manage
+    "glib2"
+    "qt5ct-kde"
+    "qt6ct-kde"
+    "kvantum-qt5"
+    "kvantum"
+    "matugen"
+
+    # hyprland
+    "hyprland"
+    "hyprpaper"
+
+    # ime
+    "fcitx5"
+    "fcitx5-configtool"
+    "fcitx5-bamboo"
+
+    # cli utils
+    "less"
+    "tree"
+    "jq"
+    "libnotify"
+    "brightnessctl"
+    "playerctl"
+
+    # apps
+    ## editor
+    "helix"
+    ## terminal
+    "kitty"
+    ## file
+    "thunar"
+    "tumbler" # thunar - thumbnail previews
+    "ffmpegthumbnailer" # thunar - video thumbnails
+    "freetype2" # thunar - font thumbnails
+    ## browser
+    "firefox"
+    ## others
+    "btop"
+    "rocm-smi-lib" # btop - amd gpu support
+    "nvtop"
+
+    # developments
+    "python"
+    "rust"
+
+    # dotfiles
+    "chezmoi"
+)
