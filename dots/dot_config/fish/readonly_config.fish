12e547d1491844f3c8925af840a71bddcb560c8c
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-01-11 23:22:59 +0700

    use single quote in fish scripts

--- dots/dot_config/fish/readonly_config.fish
+++ dots/dot_config/fish/readonly_config.fish
@@ -1,8 +1,8 @@
 if status is-interactive
     set -g fish_greeting
 
-    alias hx="helix"
-    alias lg="lazygit"
+    alias hx='helix'
+    alias lg='lazygit'
 
     fzf --fish | source
     starship init fish | source
