dd8704e7057e4b4478c6573057fba08ac15913b4
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-04-16 00:00:17 +0700

    dots/nvim: call `require` one time

--- eggs/nvim/lua/plugins/mini-icons.lua
+++ eggs/nvim/lua/plugins/mini-icons.lua
@@ -3,7 +3,8 @@ if not vim.g.is_tty then
     vim.pack.add({{ src = utils.gh("nvim-mini/mini.icons"), version = "stable" }})
 
     -- config
-    require("mini.icons").setup()
-    require("mini.icons").mock_nvim_web_devicons()
-    require("mini.icons").tweak_lsp_kind()
+    local mini_icons = require("mini.icons")
+    mini_icons.setup()
+    mini_icons.mock_nvim_web_devicons()
+    mini_icons.tweak_lsp_kind()
 end
