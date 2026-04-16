db968a6382da4d5a32d5d8fde90a5ae87580fb3e
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-04-16 18:53:33 +0700

    dots/nvim: add `tree-sitter-manager` plugin

---
+++ eggs/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,8 @@
+local utils = require("my.utils")
+vim.pack.add({ utils.gh("romus204/tree-sitter-manager.nvim") })
+
+-- config
+require("tree-sitter-manager").setup({
+    auto_install = true,
+    highlight = true,
+})
