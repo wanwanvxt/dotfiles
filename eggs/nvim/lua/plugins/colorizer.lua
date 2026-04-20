7b992787341a28e56c697c0b89078fc5a45bbad4
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-04-20 20:01:04 +0700

    dots/nvim: add `colorizer` plugin

---
+++ eggs/nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,15 @@
+local utils = require("my.utils")
+vim.pack.add({ utils.gh("norcalli/nvim-colorizer.lua") })
+
+-- config
+require("colorizer").setup({}, {
+    RGB      = true;
+    RRGGBB   = true;
+    names    = true;
+    RRGGBBAA = true;
+    rgb_fn   = true;
+    hsl_fn   = true;
+    mode     = "background";
+})
+
+vim.keymap.set("n", "<leader>c", "<cmd>ColorizerToggle<cr>", { silent = true, desc = "toggle colorizer" })
