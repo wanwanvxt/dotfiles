ba339cc95c48feb86e0e8aa2a81d84130e5214d1
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-04-20 17:53:23 +0700

    dots/nvim: using `netrw` instead

--- eggs/nvim/lua/plugins/lsp/init.lua
+++ eggs/nvim/lua/plugins/lsp/init.lua
@@ -28,13 +28,11 @@ vim.diagnostic.config({
 vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(event)
         local bufnr = event.buf
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { silent = true, buffer = bufnr })
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, { silent = true, buffer = bufnr })
-        vim.keymap.set("n", "K", function()
-            vim.lsp.buf.hover({ border = vim.o.winborder})
-        end, { silent = true, buffer = bufnr })
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename,      { silent = true, buffer = bufnr })
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { silent = true, buffer = bufnr })
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { silent = true, buffer = bufnr, desc = "goto definition" })
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, { silent = true, buffer = bufnr, desc = "goto references" })
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover({ border = vim.o.winborder}) end, { silent = true, buffer = bufnr, desc = "hover documentation" })
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename,      { silent = true, buffer = bufnr, desc = "rename symbol" })
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { silent = true, buffer = bufnr, desc = "choose code action" })
     end
 })
 
