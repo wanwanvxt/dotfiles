a0978c05dff0d4ec129846b2b7604896041ba90d
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-04-20 20:01:04 +0700

    dots/nvim: add `colorizer` plugin

--- eggs/nvim/lua/plugins/init.lua
+++ eggs/nvim/lua/plugins/init.lua
@@ -1,12 +1,13 @@
 require("plugins.kanagawa")
+require("plugins.colorizer")
+
+require("plugins.autoclose")
+require("plugins.blankline")
 
 require("plugins.heirline")
 require("plugins.which-key")
 require("plugins.neo-tree")
 require("plugins.fzf")
-
-require("plugins.autoclose")
-require("plugins.blankline")
 
 require("plugins.cmp")
 require("plugins.treesitter")
