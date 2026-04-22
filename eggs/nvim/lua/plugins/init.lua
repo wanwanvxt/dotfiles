8385b67c80f59a4b2cdfa8d5fe1c53c8c130343c
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-04-22 23:38:04 +0700

    dots/nvim: change how to load plugin

--- eggs/nvim/lua/plugins/init.lua
+++ eggs/nvim/lua/plugins/init.lua
@@ -1,14 +1,13 @@
-require("plugins.kanagawa")
-require("plugins.colorizer")
+local plugins = {
+    "kanagawa", "autoclose", "blankline",
+    "heirline", "which-key", "neo-tree", "fzf",
+    "cmp", "treesitter", "lsp", "colorizer",
+}
 
-require("plugins.autoclose")
-require("plugins.blankline")
-
-require("plugins.heirline")
-require("plugins.which-key")
-require("plugins.neo-tree")
-require("plugins.fzf")
-
-require("plugins.cmp")
-require("plugins.treesitter")
-require("plugins.lsp")
+for _, plg in ipairs(plugins) do
+    local ok = pcall(require, "plugins." .. plg)
+    if not ok then
+        local msg = string.format("Plugin '%s' could not be loaded!", plg)
+        vim.notify(msg, vim.log.levels.ERROR, { title = "nvim-config" })
+    end
+end
