89cbef8e8243155069bc1faefd4a0cf1e3c1b4eb
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-04-22 21:38:14 +0700

    dots/nvim: not load `colorizer` on tty

--- eggs/nvim/lua/plugins/colorizer.lua
+++ eggs/nvim/lua/plugins/colorizer.lua
@@ -1,15 +1,17 @@
-local utils = require("my.utils")
-vim.pack.add({ utils.gh("norcalli/nvim-colorizer.lua") })
+if not vim.g.is_tty then
+    local utils = require("my.utils")
+    vim.pack.add({ utils.gh("norcalli/nvim-colorizer.lua") })
 
--- config
-require("colorizer").setup({}, {
-    RGB      = true;
-    RRGGBB   = true;
-    names    = true;
-    RRGGBBAA = true;
-    rgb_fn   = true;
-    hsl_fn   = true;
-    mode     = "background";
-})
+    -- config
+    require("colorizer").setup({}, {
+        RGB      = true;
+        RRGGBB   = true;
+        names    = true;
+        RRGGBBAA = true;
+        rgb_fn   = true;
+        hsl_fn   = true;
+        mode     = "background";
+    })
 
-vim.keymap.set("n", "<leader>c", "<cmd>ColorizerToggle<cr>", { silent = true, desc = "toggle colorizer" })
+    vim.keymap.set("n", "<leader>c", "<cmd>ColorizerToggle<cr>", { silent = true, desc = "toggle colorizer" })
+end
