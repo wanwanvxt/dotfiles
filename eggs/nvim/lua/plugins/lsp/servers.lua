71d0ad34af5727e6e16cbebfef8f13e8bae028d9
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-05-05 18:27:28 +0700

    dots/nvim: add `vimls` lsp

--- eggs/nvim/lua/plugins/lsp/servers.lua
+++ eggs/nvim/lua/plugins/lsp/servers.lua
@@ -1,9 +1,10 @@
 return {
-    bashls = { cmd = { "bash-language-server", "start" } },
+    bashls   = { cmd = { "bash-language-server", "start" } },
     fish_lsp = { cmd = { "fish-lsp", "start" } },
-    lua_ls = { cmd = { "lua-language-server" } },
-    pylsp = { cmd = { "pylsp" } },
-    clangd = { cmd = { "clangd" } },
-    qmlls = { cmd = { "qmlls6" } },
-    ols = { cmd = { "ols" } },
+    vimls    = { cmd = { "vim-language-server", "--stdio" } },
+    lua_ls   = { cmd = { "lua-language-server" } },
+    pylsp    = { cmd = { "pylsp" } },
+    clangd   = { cmd = { "clangd" } },
+    qmlls    = { cmd = { "qmlls6" } },
+    ols      = { cmd = { "ols" } },
 }
