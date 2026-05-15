a911246f1c4d505cbfaae8f2969b2ad27f21192d
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-05-15 16:25:21 +0700

    dots/nvim: use lazy load for treesitter

--- eggs/nvim/lua/plugins/treesitter.lua
+++ eggs/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,16 @@
 local utils = require("my.utils")
-vim.pack.add({ utils.gh("romus204/tree-sitter-manager.nvim") })
 
--- config
-require("tree-sitter-manager").setup({
-    auto_install = false,
-    highlight = true,
+utils.lazy({
+    name = "treesitter",
+    packs = { utils.gh("romus204/tree-sitter-manager.nvim") },
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = "TSManager",
+    config = function()
+        require("tree-sitter-manager").setup({
+            auto_install = false,
+            highlight = true,
+        })
+
+        vim.keymap.set("n", "<leader>t", "<cmd>TSManager<cr>", { silent = true, desc = "open treesitter manager" })
+    end,
 })
-
-vim.keymap.set("n", "<leader>t", "<cmd>TSManager<cr>", { silent = true, desc = "open treesitter manager" })
