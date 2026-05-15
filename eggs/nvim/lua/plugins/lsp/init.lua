0155264d62a8fd52a7696519be282851d4655f66
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-05-15 12:39:43 +0700

    dots/nvim: add hint icon in lsp

--- eggs/nvim/lua/plugins/lsp/init.lua
+++ eggs/nvim/lua/plugins/lsp/init.lua
@@ -20,7 +20,7 @@ vim.diagnostic.config({
             [vim.diagnostic.severity.ERROR] = "E",
             [vim.diagnostic.severity.WARN]  = "W",
             [vim.diagnostic.severity.INFO]  = "I",
-            [vim.diagnostic.severity.HINT]  = "I",
+            [vim.diagnostic.severity.HINT]  = "H",
         },
     },
 })
