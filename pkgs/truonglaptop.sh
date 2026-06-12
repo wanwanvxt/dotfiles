1b75716290ae68c16fd0cb07e1cbdacf80f4103d
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-06-12 14:16:43 +0700

    pkgs: update packages list

--- pkgs/truonglaptop.sh
+++ pkgs/truonglaptop.sh
@@ -1,37 +1,34 @@
 packages=(
-    base base-devel linux linux-headers linux-zen linux-zen-headers
-    linux-firmware sof-firmware amd-ucode efibootmgr grub terminus-font
-    pacman pacman-contrib yay rate-mirrors
+    base{,-devel} linux{,-headers} linux-zen{,-headers}
+    {linux,sof}-firmware amd-ucode efibootmgr grub terminus-font
+    pacman{,-contrib} yay rate-mirrors
 
     # network & bluetooth
-    networkmanager openssh zapret2 bluez bluez-utils
+    networkmanager openssh zapret2 bluez{,-utils}
 
     # audio (pipewire)
-    rtkit pipewire pipewire-audio wireplumber
-    pipewire-alsa pipewire-pulse pipewire-jack
+    rtkit pipewire{,-audio,-alsa,-pulse,-jack} wireplumber
 
     # graphics drivers
-    mesa mesa-utils vulkan-icd-loader vulkan-tools vulkan-radeon
-    lib32-mesa lib32-vulkan-icd-loader lib32-vulkan-radeon
-    nvidia-open-dkms nvidia-utils nvidia-prime lib32-nvidia-utils
+    mesa{,-utils} vulkan-{icd-loader,tools,radeon} nvidia-{open-dkms,utils,prime}
+    lib32-{mesa,vulkan-{icd-loader,radeon},nvidia-utils}
 
     # clis
-    bash bash-completion fish git git-lfs lazygit neovim neovim-symlinks man-db
-    starship fzf chezmoi less tree ripgrep jq htop nvtop gdu
-    trash-cli xdg-utils xdg-user-dirs xdg-sound wl-clipboard
+    bash{,-completion} fish git{,-lfs} lazygit neovim{,-symlinks} man-db
+    starship fzf chezmoi less tree ripgrep jq htop nvtop gdu 7zip unarchiver
+    trash-cli xdg-{utils,user-dirs,sound} wl-clipboard
 
     # window manager (niri)
-    greetd greetd-tuigreet uwsm niri xwayland-satellite
-    xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-wlr
-    quickshell qml-niri brightnessctl playerctl
+    greetd{,-tuigreet} uwsm niri xwayland-satellite xdg-desktop-portal{,-gtk,-wlr}
+    {quickshell,qml-niri} brightnessctl playerctl
     ## looks & feel
-    noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-liberation ttf-0xproto-nerd
+    noto-fonts{,-cjk,-emoji} ttf-{liberation,0xproto-nerd}
     bibata-cursor-theme-bin papirus-icon-theme
-    adw-gtk-theme glib2 dconf # GTK
-    qt6ct-kde qt5ct-kde # Qt
+    {adw-gtk-theme,glib2,dconf} qt{6,5}ct-kde
     ## programs
-    fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool fcitx5-bamboo
-    kitty firefox nemo nemo-fileroller file-roller imv mpv
+    fcitx5{,-gtk,-qt,-configtool,-bamboo} kitty firefox imv mpv
+    {thunar{,-archive-plugin,-volman},gvfs,tumbler,ffmpegthumbnailer}
+    steam protonplus
 )
 
 services=()
