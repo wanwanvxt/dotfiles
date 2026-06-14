ca02f32df570c10e3127f31bd92f9f6dcb1632a4
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-06-14 09:56:35 +0700

    dots/nvim: move keymap config for TSManager to outsite

--- dots/dot_config/nvim/exact_lua/cfgs/treesitter.lua
+++ dots/dot_config/nvim/exact_lua/cfgs/treesitter.lua
@@ -10,7 +10,7 @@ utils.lazy({
             auto_install = false,
             highlight = true,
         })
-
-        vim.keymap.set("n", "<leader>t", "<cmd>TSManager<cr>", { silent = true, desc = "open treesitter manager" })
     end,
 })
+
+vim.keymap.set("n", "<leader>t", "<cmd>TSManager<cr>", { silent = true, desc = "open treesitter manager" })
