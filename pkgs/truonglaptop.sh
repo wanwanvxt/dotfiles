fc95a97e38594b8491835425a1f0158b11ccb14c
Vũ Xuân Trường <wanwan.vxt@gmail.com>
2026-06-14 15:29:41 +0700

    pkgs: add `gamescope` package

--- pkgs/truonglaptop.sh
+++ pkgs/truonglaptop.sh
@@ -2,8 +2,6 @@ packages=(
     base{,-devel} linux{,-headers} linux-zen{,-headers}
     {linux,sof}-firmware amd-ucode efibootmgr grub terminus-font
     pacman{,-contrib} yay rate-mirrors
-    ## performance
-    zram-generator power-profiles-daemon
 
     # network & bluetooth
     networkmanager openssh zapret2 bluez{,-utils}
@@ -19,7 +17,8 @@ packages=(
     bash{,-completion} fish git{,-lfs} lazygit neovim{,-symlinks} man-db
     starship fzf chezmoi less tree ripgrep jq htop nvtop gdu 7zip unrar
     trash-cli xdg-{utils,user-dirs,sound,terminal-exec} wl-clipboard
-    ##
+    ## performance
+    zram-generator power-profiles-daemon
 
     # window manager (niri)
     greetd{,-tuigreet} uwsm niri xwayland-satellite xdg-desktop-portal{,-gtk,-wlr}
@@ -31,7 +30,7 @@ packages=(
     ## programs
     fcitx5{,-gtk,-qt,-configtool,-bamboo} kitty firefox imv mpv{,-mpris,-modernz,-thumbfast-git}
     {thunar{,-archive-plugin,-volman},gvfs,tumbler,ffmpegthumbnailer} xarchiver
-    steam protonplus gamemode mangohud
+    steam protonplus gamescope gamemode mangohud
 )
 
 services=(
